abe77f2f2cd18f437c6f252842c1db324f7b4651
BeNegrette <135248789+BeNegrette@users.noreply.github.com>
2023-06-09 14:50:46 +0200

    login.php

---
+++ login.php
@@ -0,0 +1,8 @@
+<form action="registro.php" method="POST">
+  <!-- Campos del formulario -->
+  <input type="text" name="nombre" placeholder="Nombre">
+  <input type="email" name="email" placeholder="Email">
+  <input type="password" name="contraseña" placeholder="Contraseña">
+  <!-- Resto del formulario -->
+  <button type="submit">Registrarse</button>
+</form>
